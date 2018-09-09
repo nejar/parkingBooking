73b1314fd9ae44f3958cfa263606cc4c32708b80
nejar <nejarshrestha123@gmail.com>
2018-09-09 21:08:06 +0545

    asdf

--- OnlineParkingBooking/views/bookForm.php
+++ OnlineParkingBooking/views/bookForm.php
@@ -2,20 +2,38 @@
 include_once'header.php';
 ?>
 <div class="booking">
-	<div class="form-wrapper">
+<!-- 	<div class="form-wrapper">
 		<form action="">
 			<table border="1">
-				<tr>
+				<tr class="gap">
 					<th colspan="2"><label for="bookingDate">Booking Date</label></th>
 					<td><input type="Date" name="bookingDate"></td>
 				</tr>
-				<tr>
+				<tr class="gap">
 					<th colspan="2"><label for="vehicleNo">Vehicle Number</label></th>
 					<td><input type="text" name="vehicleNo" placeholder="Enter Vehicle No."></td>
 				</tr>
+				<tr class="gap">
+					<th colspan="2"></th>
+					<td><input type="submit" class="button" name="submit"></td>
+				</tr>
 			</table>
 		</form>
-	</div>
+	</div> -->
+
+<form action="doit" id="doit" method="post">
+    <label class="gap">
+        Booking Date
+        <input type="Date" name="bookingDate">
+    </label>
+
+    <label>
+        Vehicle Number
+        <input type="text" name="vehicleNo" placeholder="Enter Vehicle No." />
+    </label>
+    <input type="submit" class="button" name="submit">
+</form>
+
 </div>
 
 
